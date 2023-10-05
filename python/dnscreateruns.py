9751705157fc19884e67fa3517ce03c8e1bbb801
Gökhan Yalnız <gokhan.yalniz@ist.ac.at>
2023-10-05 15:20:25 +0200

    added a script for quickly creating many run folders.

---
+++ python/dnscreateruns.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import argparse
+from pathlib import Path
+import numpy as np
+from os import makedirs
+from shutil import copy
+
+import dns
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Create run folders.",
+    )
+    parser.add_argument("parentdir", type=str, help="path to the parent dir for runs")
+    parser.add_argument(
+        "Re_i",
+        type=float,
+        dest="Rei",
+    )
+    parser.add_argument(
+        "Re_f",
+        type=float,
+        dest="Ref",
+    )
+    parser.add_argument(
+        "Re_delta",
+        type=float,
+        dest="Ref",
+    )
+
+    args = vars(parser.parse_args())
+    dnscreateruns(**args)
+
+
+def dnscreateruns(parentdir, Re_i, Re_f, Re_delta):
+
+    parentdir = Path(parentdir)
+    paramsfile = parentdir / "parameters.in"
+    icfile = parentdir / "state.000000"
+    slurmfile = parentdir / "dns.slurm"
+
+    parameters = dns.readParameters(paramsfile)
+    parameters["initiation"]["ic"] = 0
+    parameters["initiation"]["i_start"] = 0
+    parameters["initiation"]["t_start"] = 0
+
+    Res = np.arange(Re_i, Re_f, Re_delta)
+    for iRe in range(Res):
+        Re = Res[iRe]
+        dirname = f"re{Re:.2f}"
+        outdir = parentdir / dirname
+
+        makedirs(outdir)
+        copy(icfile, outdir)
+        copy(slurmfile, outdir)
+
+        parameters["physics"]["re"] = Re
+        dns.writeParameters(parameters, outdir / "parameters.in")
+
+if __name__ == "__main__":
+    main()
